d123bb6a9208338f9f55474278d10b230804de31
MohammadMahdi Ahangaran <m.mahdi2414@gmail.com>
2020-07-16 19:19:03 +0430

    change toJSON of form answers model

--- formAnswer/api.js
+++ formAnswer/api.js
@@ -29,7 +29,7 @@ router.get('/' , (req , res)=>{
 router.post('/:id' , (req , res)=> {
     let answer = req.body;
     const id = req.params.id;
-
+    answer['value'] = {...answer};
     answer['formId'] = id;
     answer['createAt'] = new Date();
     const resultPromise = service.createFormAnswer(answer);
--- formAnswer/model.js
+++ formAnswer/model.js
@@ -19,9 +19,7 @@ const formAnswerSchema = new Schema({
 {
   toJSON: {
     transform: function (doc, ret) {
-      ret.id = ret._id;
-      delete ret._id;
-      delete ret.__v;
+      return ret = {id:ret._id , createAt: ret.createAt , ...ret.value};
     }
   }
 });

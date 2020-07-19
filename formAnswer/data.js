636c2b2d7072e3cd75c8df25de16a7667e6992d3
MohammadMahdi Ahangaran <m.mahdi2414@gmail.com>
2020-07-19 13:41:13 +0430

    validation form answers and sort by creatAt

--- formAnswer/data.js
+++ formAnswer/data.js
@@ -2,7 +2,7 @@ const FormAnswer = require('./model');
 const Form = require('./../form/model');
 
 let findAllAnswers = async ()=> {
-    return await FormAnswer.find().populate('formId');
+    return await FormAnswer.find().populate({path: 'formId', options: { sort: { 'createdAt': -1}}});
 }
 
 let findAnswer = async (id)=> {
@@ -20,7 +20,16 @@ let createFormAnswer = async (formAnswerJson) => {
     let promise = new Promise((resolve , reject)=>{
         Form.findById(formAnswerJson.formId).then(res=>{
             if (res){
-                resolve(formAnswer.save());
+                let ok = true;
+                res.fields.forEach(field => {
+                    if (field.required && !formAnswerJson.values[field.name]){
+                        ok = false;
+                        reject({status:422 , body: {message:"form answer is not complete"}});
+                        return;
+                    }
+                });
+                if (ok === true)
+                    resolve(formAnswer.save());
             }
             else{
                 reject({status:404 , body:{message:`not find form with id = ${formAnswerJson.formId}`}});

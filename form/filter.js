172a9bd112e64cfd68c8cec61b765bf68e65fb24
MohammadMahdi Ahangaran <m.mahdi2414@gmail.com>
2020-07-22 21:55:31 +0430

    date

--- form/filter.js
+++ form/filter.js
@@ -5,16 +5,33 @@ let filteredBy = (form , filter)=>{
         for(const field of form.fields){
             if (!filter[field.name])
                 continue;
+            if (!answer.value)
+            {
+                result.pop();
+                break;
+            }
             if (field.type === 'Number'){
                 if (parseInt(answer.value) <= parseInt(filter[field.name].to) && parseInt(answer.value) >= parseInt(filter[field.name].from))
                     continue;
             }
             else if (field.type === 'Text'){
-                if (answer.value.includes(filter[field.name]))
+                let ok = false;
+                for(const pat of filter[field.name])
+                {
+                    if (answer.value.includes(pat)){
+                        ok = true;
+                        break;
+                    }
+                };
+                if (ok === true)
                     continue;
             }
             else if (field.type === 'Data'){
-                continue;
+                let date = new Date(answer.value);
+                let from = new Date(filter[field.name].from);
+                let to = new Date(filter[field.name].to);
+                if (parseInt(date.getTime()) <= parseInt(to.getTime()) && parseInt(date.getTime()) >= parseInt(from.getTime()))
+                    continue;
             }
             else {
                 continue;

77cbff5660338cf8b9831c610d386c87c3820a3d
MohammadMahdi Ahangaran <m.mahdi2414@gmail.com>
2020-07-23 01:45:36 +0430

    try again

--- app.js
+++ app.js
@@ -17,18 +17,18 @@ app.use(express.json());
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods","GET,HEAD,OPTIONS,POST,PUT,DELETE");
-  res.setHeader("Access-Control-Allow-Headers",
-  "Origin,Cache-Control,Accept,X-Access-Token ,X-Requested-With, Content-Type, Authorization, Access-Control-Request-Method"
-  );
-  if (req.method === "OPTIONS") {
-      return res.status(200).end();
-  }
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   res.setHeader("Access-Control-Allow-Methods","GET,HEAD,OPTIONS,POST,PUT,DELETE");
+//   res.setHeader("Access-Control-Allow-Headers",
+//   "Origin,Cache-Control,Accept,X-Access-Token ,X-Requested-With, Content-Type, Authorization, Access-Control-Request-Method"
+//   );
+//   if (req.method === "OPTIONS") {
+//       return res.status(200).end();
+//   }
+//   next();
+// });
 
 const session = {
   secret: process.env.AUTH0_SESSION_SECRET,

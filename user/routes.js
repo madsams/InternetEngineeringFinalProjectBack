82ec6168a85b8e645570aedcb1ea1657e65af0a1
Armin <armin.mazloomi@gmail.com>
2020-07-22 21:37:03 +0430

    user role api added

--- user/routes.js
+++ user/routes.js
@@ -9,27 +9,17 @@ AUTH0_MGMT_API_ACCESS_TOKEN = process.env.AUTH0_MGMT_API_ACCESS_TOKEN;
 
 const roles = defaultRoles;
 
-// log('out');
-// log(roles);
-
 let apiManagementHeaders = {
 	headers: {
 		Authorization: `Bearer ${AUTH0_MGMT_API_ACCESS_TOKEN}`
 	}
 }
 
-// router.get('/roles', (req, res) => {
-// 	getRoles().then(roles=>{
-// 		return res.status(200).json(roles);
-// 	})
-// });
-
 router.get('/roles', (req, res) => {
-	log('info' , req.user);
-	log('info' , `id: ${req.user.id}`);
 	let url = `https://ieng-final-project.eu.auth0.com/api/v2/users/${req.user.id}/roles`;
 	axios.get(url,apiManagementHeaders).then((result) =>{
-		return res.status(200).json(result);
+		console.log(result);
+		return res.status(200).json(result.data);
 	}).catch((err)=> {
 		res.status(400).json(err);
 	});

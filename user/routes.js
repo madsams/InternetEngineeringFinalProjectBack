15db1fcfc8b5480331d56d33ca41e836038c3b50
Mohammad Seyfayi <madsams024@gmail.com>
2020-07-23 11:16:47 +0430

    jwt added

--- user/routes.js
+++ user/routes.js
@@ -18,12 +18,13 @@ let apiManagementHeaders = {
 
 router.get('/roles', (req, res) => {
   let userId = req.user.sub.split('|')[1];
-  console.log(userId);
   let url = `https://ieng-final-project.eu.auth0.com/api/v2/users/${userId}/roles`;
   axios.get(url, apiManagementHeaders).then((result) => {
     console.log(result);
     return res.status(200).json(result.data);
   }).catch((err) => {
+    console.log(err);
+    console.log(url);
     res.status(400).json(err);
   });
 });
